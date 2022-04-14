5a586c14d0d47d62aa5585cb1c6fd850570af141
ah13558 <ah13558@bp1-login03.data.bp.acrc.priv>
2022-04-14 15:49:18 +0100

    Changing to use hydra initialize

--- train.py
+++ train.py
@@ -1,11 +1,12 @@
 import os
-
+import gc
 import numpy as np
 
 import torch
 import torchvision
 import torchvision.datasets as datasets
 
+from hydra import compose, initialize
 from omegaconf import DictConfig, OmegaConf
 import hydra
 import wandb
@@ -52,17 +53,18 @@ def update_plots(in_image, out_image, iteration, mode='train'):
             'test_recon': [wandb.Image(out_grid, caption="Reconstructed Test Images")],
             })
 
+from torch.utils.data.distributed import DistributedSampler
 
 def configure_datasets(cfg):
     if cfg.dataset.type == 'h5':
         train_dataloader = torch.utils.data.DataLoader(
-            h5Dataset(cfg.dataset.dir.train),
+            DistributedSampler(h5Dataset(cfg.dataset.dir.train)),
             batch_size=cfg.dataset.batch_size,
             num_workers=cfg.dataset.num_workers,
             shuffle=True)
     elif cfg.dataset.type == 'noise':
         train_dataloader=torch.utils.data.DataLoader(
-            NoiseDataset(min=cfg.dataset.min, max=cfg.dataset.max),
+            DistributedSampler(NoiseDataset(min=cfg.dataset.min, max=cfg.dataset.max)),
             batch_size=cfg.dataset.test_batch_size,
             num_workers=cfg.dataset.num_workers,
             shuffle=False)
@@ -88,10 +90,18 @@ def main(cfg) -> None:
     hydra_path = os.getcwd()
     wandb.init(project='denoising', config=cfg)
     device = 'cuda' if torch.cuda.is_available() and not cfg.no_cuda else 'cpu'
+    torch.distributed.init_process_group(backend="nccl")
+    import os
+    local_rank = torch.distributed.get_rank()
+    torch.cuda.set_device(local_rank)
+    device = torch.device("cuda", local_rank)
+
     train_dataloader, test_dataloader = configure_datasets(cfg)
 
     model = BF_CNN(cfg.model)
-    model = model.to(device)
+    model.to(device)
+    import torch.nn as nn
+    model = nn.parallel.DistributedDataParallel(model, device_ids=[local_rank], output_device=local_rank)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=cfg.lr)
     scheduler = torch.optim.lr_scheduler.MultiStepLR(
@@ -103,7 +113,7 @@ def main(cfg) -> None:
     for epoch in range(cfg.epochs):
         model.train()
         for i, inputs in enumerate(train_dataloader):
-            inputs = inputs.to(device)
+            inputs = inputs.to('cuda')
             noisy_input = inputs + get_noise(inputs)
             outputs = noisy_input - model(noisy_input)
             loss = loss_fn(outputs, inputs)
@@ -122,7 +132,7 @@ def main(cfg) -> None:
                     f'\tLoss: {loss.item():.3f} |'
                     f'\tPSNR: {p:.3f} |'
                     f'\tSSIM: {m:.3f} |')
-                update_logs(loss.item, p, m, iteration, optimizer)
+                update_logs(loss.item(), p, m, iteration, optimizer)
 
             if iteration % cfg.eval_iter == 0:
                 model.eval()
@@ -130,19 +140,39 @@ def main(cfg) -> None:
                 for test_batch in test_dataloader:
                     test_batch = test_batch.to(device)
                     noisy_test = test_batch + get_noise(test_batch)
-                    outputs = noisy_test - model(noisy_test)
-                    test_losses.append(loss_fn(outputs, test_batch).item())
-                    test_psnr.append(psnr(outputs, test_batch))
-                    test_ssim.append(ssim(outputs, test_batch))
+                    test_outputs = noisy_test - model(noisy_test)
+                    test_losses.append(loss_fn(test_outputs, test_batch).item())
+                    test_psnr.append(psnr(test_outputs, test_batch))
+                    test_ssim.append(ssim(test_outputs, test_batch))
+
                 update_logs(
                     np.mean(test_losses), np.mean(test_psnr), np.mean(test_ssim), iteration, optimizer, mode='test')
-                update_plots(noisy_test[0], outputs[0], mode='test')
-                
+                update_plots(noisy_test[0], test_outputs[0], iteration, mode='test')
+
+                del test_batch
+                del noisy_test
+                del test_outputs
+
+                # Save model every epoch
+                torch.save(model.module.state_dict(), '%d-%d-checkpoint.pth'%(epoch, iteration))
+
                 model.train()
+            gc.collect()
+
+            del inputs
+            del noisy_input
+            del outputs
+
             iteration += 1
 
-        # Save model every epoch
-        torch.save(model.state_dict(), '%d-%d-checkpoint.pth'%(epoch, iteration))
-
 if __name__ == "__main__":
-    main()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--local_rank", type=int)
+    parser.add_argument("--config_dir", type=str)
+    parser.add_argument("--config_name", type=str)
+    args = parser.parse_args()
+
+    initialize(config_path=args.config_dir)
+    cfg = compose(config_name=args.config_name)
+    main(cfg)
